10b31816c310c63befc2f044018fc531c0c6e4b8
Théo <Tho@10.5.208.245>
2015-12-04 14:49:43 +0100

    userDAO + qqs queries

--- Projet_IT/src/com/ecetech/b3/projetit/common/SQLQueries.java
+++ Projet_IT/src/com/ecetech/b3/projetit/common/SQLQueries.java
@@ -1,5 +1,14 @@
 package com.ecetech.b3.projetit.common;
 
 public class SQLQueries {
-
+	
+	/*queries for Category*/
+	
+	/*queries for Comment*/
+	
+	/*queries for Event*/
+	
+	/*queries for User*/
+	public static final String GET_USER_QUERY = "SELECT DISTINCT * FROM 'user' WHERE user.idUser = ";
+	public static final String GET_ALL_USER_QUERY = "SELECT DISTINCT * FROM 'user'";
 }
--- Projet_IT/src/com/ecetech/b3/projetit/dao/UserDAO.java
+++ Projet_IT/src/com/ecetech/b3/projetit/dao/UserDAO.java
@@ -1,23 +1,43 @@
 package com.ecetech.b3.projetit.dao;
 
+import java.sql.*;
 import java.util.ArrayList;
 
-import com.ecetech.b3.projetit.beans.User;
+import com.ecetech.b3.projetit.beans.*;
+import com.ecetech.b3.projetit.connection.*;
+import com.ecetech.b3.projetit.common.*;
+
 
 public class UserDAO {
-
-	private User usr;
-
-	public UserDAO() {
-		super();
-	}
-
-	public User getUsr(String name, String psw) {
-		User usr = null;
-		return usr;
-	}
 	
-	public ArrayList<User> getAllUser() {
-		return null;
+	private Connection connection;
+	private Statement statement;
+	private User user;
+
+	public User getUser(int idUser) throws SQLException {
+		String query = SQLQueries.GET_USER_QUERY + idUser;
+		ResultSet rs = null;
+		User user = null;
+		try {
+			connection = Connector.getConnection();
+			statement = connection.createStatement();
+			rs = statement.executeQuery(query);
+			if (rs.next()) {
+				user = new User();
+				user.setIdUser(rs.getInt("idUser"));
+				user.setMailUser(rs.getString("mailUser"));
+				user.setNomUser(rs.getString("nomUser"));
+				user.setPasswordUser(rs.getString("passwordUser"));
+				user.setOkUser(rs.getBoolean("okUser"));
+				user.setAdminUser(rs.getBoolean("adminUser"));
+			}
+		} finally {
+			if (statement != null)
+				statement.close();
+			if (rs != null)
+				rs.close();
+			connection.close();
+		}
+		return user;
 	}
 }
